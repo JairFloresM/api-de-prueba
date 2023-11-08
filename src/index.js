c0f112758f37d8c6b2f1ac1412a36fd6cdc5e5ae
JairFloresM <jairfm01@gmail.com>
2023-11-08 11:49:43 -0500

    se agrego un error para que lo detecte sonarqube

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ app.get('/api', (req, res) => {
 
 //ruta de la api
 app.get('/sonar', (req, res) => {
+    // Error deliberado: falta de manejo de error
+    throw new Error('Simulated error');
     res.json(
         {
             message: "SonarQube",
